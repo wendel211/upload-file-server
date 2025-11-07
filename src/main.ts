40047f47b8b67b0c8f11ef176c83c06a419e35e9
wendel211 <wendelmuniz04@gmail.com>
2025-11-06 23:44:51 -0300

    feat: add @nestjs/config and @nestjs/swagger dependencies, configure Swagger and CORS in main application

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,42 @@
-import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { UploadsModule } from './uploads/uploads.module';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-@Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
-      serveRoot: '/uploads',
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, 
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
-    UploadsModule,
-  ],
-})
-export class AppModule {}
+  );
+
+  // 🔹 Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Upload e Servidor de Arquivos')
+    .setDescription(
+      'API NestJS para upload e gerenciamento de arquivos estáticos (imagens, PDFs, etc).',
+    )
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // 🔹 Permitir CORS (caso o front use o servidor)
+  app.enableCors();
+
+  // 🔹 Porta padrão
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  console.log(`🚀 Aplicação rodando em: http://localhost:${PORT}`);
+  console.log(`📘 Swagger disponível em: http://localhost:${PORT}/api`);
+}
+
+bootstrap();
